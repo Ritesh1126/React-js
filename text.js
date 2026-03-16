06489b63d36dc5edfee42c87251a3f21d0679b6b
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-03-16 23:23:38 +0530

    commenting to start new learning

--- text.js
+++ text.js
@@ -1,8 +1,9 @@
-const cirlce = () => {
-    return React.createElement(
-        'circle', {
-            id: 'circle'
-        }, 'This is a circle '
-    )
-}
-export default circle;
+// Part 1 : lect 67 
+// const cirlce = () => {
+//     return React.createElement(
+//         'circle', {
+//             id: 'circle'
+//         }, 'This is a circle '
+//     )
+// }
+// export default circle;
